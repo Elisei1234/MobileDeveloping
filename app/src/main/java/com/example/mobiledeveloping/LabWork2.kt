1296c84451d189a0a048adc19ccd64ee40a1f279
user-IVC <eliseisiniaev@yandex.ru>
2024-09-21 12:07:36 +0300

    Этот коммит только для меня

--- app/src/main/java/com/example/mobiledeveloping/LabWork2.kt
+++ app/src/main/java/com/example/mobiledeveloping/LabWork2.kt
@@ -1,16 +1,28 @@
 package com.example.mobiledeveloping
 
+import kotlin.random.Random
+
 fun main()
 {
-    println("Введите любое целочисленное значение")
-    var number = readln().toInt()
+    //println("Введите любое целочисленное значение")
+    //var number = readln().toInt()
     //println(task1(number))
-    println(task2(number))
-    //task3()
+    //println(task2(number))
+    println("Введите размерность массива: ")
+    var n = readln().toInt()
+    val array = IntArray(size = n)
+    for(i in 0 until array.size)
+    {
+        array[i] = (-100 until 501).random()
+    }
+
+    println("Исходный массив: ")
+    _Array(array)
+
 }
 
 
-//3.	Написать функцию. Входящий параметр (number) – целочисленная переменная.
+//Написать функцию. Входящий параметр (number) – целочисленная переменная.
 // Действие функции – если number нечетное, вернуть нуль, иначе вернуть квадрат числа number.
 fun task1(number: Int) : Int?
 {
@@ -25,10 +37,26 @@ fun task1(number: Int) : Int?
 }
 
 
-//3.	Написать лямбда-выражение. Входящий параметр Int. Выходящий Boolean.
+//Написать лямбда-выражение. Входящий параметр Int. Выходящий Boolean.
 // Действие лямбды – если входящий параметр является трехзначным числом (от 100 до 999), вернуть true, иначе вернуть false.
 
 val task2 = {num: Int ->
     if(num > 99 && num < 1000) true
     else false
 }
+
+//Task3
+fun _Array(numbers: IntArray)
+{
+    for(i in 0..numbers.size - 1)
+    {
+        print("${numbers[i]}  ")
+        // print("  ")
+    }
+}
+
+
+//Task 5.
+//1. (Int, Int, String) -> Unit.
+//2. () -> String.
+//3. () -> .
