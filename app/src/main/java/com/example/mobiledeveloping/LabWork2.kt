0317e5f9df33494f78f3aa6b5c810aa72b1a518c
user-IVC <eliseisiniaev@yandex.ru>
2024-09-21 09:19:48 +0300

    Les goal(task1 is done)

--- app/src/main/java/com/example/mobiledeveloping/LabWork2.kt
+++ app/src/main/java/com/example/mobiledeveloping/LabWork2.kt
@@ -1,11 +1,25 @@
 package com.example.mobiledeveloping
 
-fun LabWork2()
+fun main()
 {
-
+    println("Введите любое целочисленное значение")
+    var number = readln().toInt()
+    println(task1(number))
+    //task2()
+    //task3()
 }
 
-fun task1()
-{
 
+//3.	Написать функцию. Входящий параметр (number) – целочисленная переменная.
+// Действие функции – если number нечетное, вернуть нуль, иначе вернуть квадрат числа number.
+fun task1(number: Int) : Int?
+{
+    if(number % 2 !=0)
+    {
+        return null
+    }
+    else
+    {
+        return number * number
+    }
 }
