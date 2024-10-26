ac4c96923339b5c8f83bb773d3bb14eeaa4fbb6e
user-IVC <eliseisiniaev@yandex.ru>
2024-10-26 12:03:19 +0300

    1/4 of 1st video

--- app/src/main/java/com/example/mobiledeveloping/MainActivity.kt
+++ app/src/main/java/com/example/mobiledeveloping/MainActivity.kt
@@ -3,13 +3,30 @@ package com.example.mobiledeveloping
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.mobiledeveloping.ui.theme.MobileDevelopingTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,11 +48,38 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    val state = remember{
+        mutableStateOf("unknown")
+    }
+    Column(modifier = Modifier.fillMaxSize()){
+        Box(modifier = Modifier
+            .fillMaxHeight(0.5f)
+            .fillMaxWidth(),
+            contentAlignment = Alignment.Center
+        ){
+            Text(text = "Temp in $name!")
+        }
+
+        Box(modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth(),
+            contentAlignment = Alignment.BottomCenter
+        )
+        {
+            Button(onClick = {
+
+            }, modifier = Modifier
+                .padding(5.dp)
+                .fillMaxWidth()
+            ){
+                Text(text = "Refresh")
+            }
+
+            
+        }
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
