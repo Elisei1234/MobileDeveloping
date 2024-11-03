fc8f1638006b722a12fadfdc3c0579a60e5a5c34
Elisei1234 <eliseisiniaev@yandex.ru>
2024-11-03 18:53:26 +0300

    1st video - drawer_Menu

--- app/src/main/java/com/example/appinfo/MainActivity.kt
+++ app/src/main/java/com/example/appinfo/MainActivity.kt
@@ -1,47 +1,54 @@
 package com.example.appinfo
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.appinfo.ui.theme.AppInfoTheme
+import com.example.ui_components.MainTopBar
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.mutableStateOf
+import com.example.ui_components.DrawerMenu
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+
+            val scaffoldState = rememberScaffoldState()
+            val topBarTitle = remember {
+                mutableStateOf("Грибы")
+            }
             AppInfoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(
+                    scaffoldState = scaffoldState,
+                    topBar = {
+                        MainTopBar(title = topBarTitle.value,
+                            scaffoldState
+                        )
+                    },
+                    drawerContent = {
+                        DrawerMenu()
+                    }
+                ){
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AppInfoTheme {
-        Greeting("Android")
     }
 }
