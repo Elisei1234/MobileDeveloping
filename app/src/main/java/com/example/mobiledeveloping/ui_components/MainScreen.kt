67a8e43f914ece2f7e7637864254446a829e22eb
user-IVC <eliseisiniaev@yandex.ru>
2024-11-06 10:19:11 +0300

    2nd video - add background and other one

---
+++ app/src/main/java/com/example/mobiledeveloping/ui_components/MainScreen.kt
@@ -0,0 +1,71 @@
+package com.example.mobiledeveloping.ui_components
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.example.mobiledeveloping.R
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreen (){
+    Image(
+        painter  = painterResource(id = R.drawable.weather_bg),
+        contentDescription = "im1",
+        modifier = Modifier.fillMaxSize()
+            .alpha(0.5f),
+        contentScale = ContentScale.FillBounds
+    )
+    Column(
+        modifier = Modifier.fillMaxSize()
+            .padding(5.dp),
+    ) {
+        Card(
+            modifier = Modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(10.dp)
+        ){
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ){
+                    Text(
+                        modifier = Modifier.padding(top = 8.dp, start = 8.dp),
+                        text = "6 November 2024 9:53",
+                        color = Color.White
+                    )
+                    AsyncImage(
+                        model = "https://cdn.weatherapi.com/weather/64x64/day/116.png",
+                        contentDescription = "im2",
+                        modifier = Modifier.size(35.dp)
+                            .padding(top = 3.dp, end = 8.dp)
+                    )
+                }
+
+            }
+        }
+
+    }
+}
